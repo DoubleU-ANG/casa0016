f44af22967ed65b936c494d079ce4973b3a16c18
DoubleU-ANG <100694831+DoubleU-ANG@users.noreply.github.com>
2024-01-05 23:03:51 +0000

    Rain detection
    For testing rain drop sensor and servo

---
+++ water proof house/Rain_Detection/Rain_Detection.ino
@@ -0,0 +1,38 @@
+#include <Servo.h>  
+#define YELLOW 12
+
+Servo myservo;        // create a servo object 
+int rain = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(A0,INPUT);
+  pinMode(YELLOW,OUTPUT);
+  digitalWrite(YELLOW, LOW);
+  myservo.attach(9);
+  u8g2.begin();
+}
+
+void loop() {
+  analogWrite(2,(map(analogRead(A0), 0, 1023, 235, 0)));
+  rain = map(analogRead(A0), 0, 1023, 235, 0);
+  Serial.print("rain = ");
+  Serial.println(rain);
+  
+  if(rain<70&&rain>10)
+  {
+   digitalWrite(YELLOW, HIGH); 
+   myservo.write(rain);
+  }
+  else if(rain>=70)
+  {
+   digitalWrite(YELLOW, HIGH); 
+   myservo.write(70); 
+  }
+  else if(rain<=10)
+  {
+  digitalWrite(YELLOW, LOW);
+  myservo.write(0);   
+  }
+  
+}
